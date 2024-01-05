0f75c5091f3917c7cae1b7017d03a8cecd301bb2
Kadon Engle <englekadondax@gmail.com>
2024-01-05 11:39:33 -0800

    Init commit

---
+++ src/assets/data/adventureData.js
@@ -0,0 +1,13 @@
+const adventureData = [
+    {
+        title: "Sahalie Falls",
+        location: {
+            lat: 44.349117,
+            lng: -121.997223
+        },
+        imagesDirectory: 'adventure_images/sahalie_falls/',
+        description: 'We enjoyed a fun day in Portland '
+    }
+];
+
+export default adventureData;
