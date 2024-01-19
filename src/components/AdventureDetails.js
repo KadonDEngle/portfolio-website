510483efbd2488939549fb9542c55849cab00341
Kadon Engle <englekadondax@gmail.com>
2024-01-19 11:58:20 -0800

    Added logic and styles to adventure gallery

--- src/components/AdventureDetails.js
+++ src/components/AdventureDetails.js
@@ -6,30 +6,76 @@ const renderAdventureDetails = adventure => {
     const adventureDetailsHTML = `
         <i class='fa-solid fa-x adventure-details__close-btn'></i>
         <h3 class='adventure-details__title'>${adventure.title}</h3>
-        <div class='adventure-details__visits'>
+        <div class='adventure-details__dates'>
             ${adventure.visits.map((visit, index) => `
-                <button class='adventure-details__visit__date' data-index=${index}>${visit.date}</button>
-            `).join('')}
-            ${adventure.visits.map(visit => `
-                <div class='adventure-details__visit'>
-                    <div class='adventure-details__visit__images'>
-                        ${visit.images.map(image => `
-                            <img src=${image} class='adventure-details__visit__image'>
-                        `).join('')}
-                    </div>
-                </div>
+                <button class='adventure-details__date-btn' data-index=${index} ${index == 0 ? 'disabled':''}>${visit.date}</button>
             `).join('')}
         </div>
+        ${adventure.visits.map((visit, index) => `
+            <div class='adventure-details__visit ${index == 0 ? 'adventure-details__visit--visible':''}' data-index=${index}>
+                ${visit.note ? 
+                `<div class='adventure-details__note'>
+                    <p class='adventure-details__note-text'>" ${visit.note} "</p>
+                </div>` 
+                : ''}
+                <img src=${visit.images[0]} class='adventure-details__main-image'>
+                <div class='adventure-details__images'>
+                    ${visit.images.map((image, index) => `
+                        <img src=${image} class='adventure-details__image ${index == 0 ? 'adventure-details__image--selected':''}'>
+                    `).join('')}
+                </div>
+            </div>
+        `).join('')}
     `;
 
+    // Set details content and make visible
     adventureDetailsEl.innerHTML = adventureDetailsHTML;
-
     adventureDetailsEl.classList.add('adventure-details--visible');
 
-    const closeBtnEl = document.querySelector('.adventure-details__close-btn');
+    // Make sure main page doesn't scroll while details are open
+    document.body.style.overflow = 'hidden';
 
+    // Set up close button logic
+    const closeBtnEl = document.querySelector('.adventure-details__close-btn');
     closeBtnEl.addEventListener('click', () => {
         adventureDetailsEl.classList.remove('adventure-details--visible');
+
+        // Allow main page to scroll again
+        document.body.style.overflow = 'auto';
+    });
+
+    // Select the newly created 
+    const dateBtnEls = document.querySelectorAll('.adventure-details__date-btn');
+    const visitEls = document.querySelectorAll('.adventure-details__visit');
+    
+    // No need to set up listeners if there is only one visit / date
+    if (dateBtnEls.length > 1) {
+        dateBtnEls.forEach(btn => {
+            const btnIndex = btn.getAttribute('data-index');
+            btn.addEventListener('click', () => {
+                // Toggle the appropriate visit's visibility
+                visitEls.forEach(visit => {
+                    const visitIndex = visit.getAttribute('data-index');
+                    visit.classList.toggle('adventure-details__visit--visible', btnIndex == visitIndex);
+                });
+                // Disable clicked button
+                dateBtnEls.forEach(btn => btn.disabled = false);
+                btn.disabled = true;
+            });
+        });
+    }
+
+    visitEls.forEach(visit => {
+        const mainImageEl = visit.querySelector('.adventure-details__main-image');
+        const imageEls = visit.querySelectorAll('.adventure-details__image');
+    
+        imageEls.forEach(image => {
+            image.addEventListener('click', () => {
+                mainImageEl.src = image.src;
+                imageEls.forEach(image => image.classList.remove('adventure-details__image--selected'));
+                image.classList.add('adventure-details__image--selected');
+            });
+        });
     });
 };
 
--- src/data/adventureData.js
+++ src/data/adventureData.js
@@ -8,6 +8,7 @@ const adventureData = [
         visits: [
             {
                 date: "Sept 11, 2022",
+                note: "Portland trip isn't complete without Auntie Anne's and Top Golf.",
                 images: Object.values(import.meta.glob('/src/data/adventure_images/multnomah_falls/*.jpg', { eager: true })).map(module => module.default)
             }
         ]
@@ -21,6 +22,7 @@ const adventureData = [
         visits: [
             {
                 date: "Sept 17, 2022",
+                note: "Two incredibly unique waterfalls. Super fun hike.",
                 images: Object.values(import.meta.glob('/src/data/adventure_images/gold_and_silver_falls/*.jpg', { eager: true })).map(module=> module.default)
             },
         ]
@@ -34,6 +36,7 @@ const adventureData = [
         visits: [
             {
                 date: "Sept 02, 2022",
+                note: "Insanely beautiful waterfall up the McKenzie Highway. Unique approach over a lava rock trail. Timed it poorly and had to hike out in the dark.",
                 images: Object.values(import.meta.glob('/src/data/adventure_images/proxy_falls/*.jpg', { eager: true })).map(module => module.default)
             },
         ]
@@ -47,6 +50,7 @@ const adventureData = [
         visits: [
             {
                 date: "Aug 19, 2022",
+                note: "I cannot believe a waterfall this massive and beautiful has been right here and I've never visited it.",
                 images: Object.values(import.meta.glob('/src/data/adventure_images/salt_creek_falls/*.jpg', { eager: true })).map(module => module.default)
             },
         ]
@@ -60,6 +64,7 @@ const adventureData = [
         visits: [
             {
                 date: "Jan 14, 2022",
+                note: "Three step waterfall in Cascadia State Park. Much taller than I expected it to be. Beautiful campground, need to come back and stay at campsite A12.",
                 images: Object.values(import.meta.glob('/src/data/adventure_images/lower_soda_falls/*.jpg', { eager: true })).map(module => module.default)
             },
         ]
@@ -73,10 +78,12 @@ const adventureData = [
         visits: [
             {
                 date: "Apr 12, 2022",
+                note: "First time taking my soulmate to Smith Rock! <3",
                 images: Object.values(import.meta.glob('/src/data/adventure_images/smith_rock/04-12-22/*.jpg', { eager: true })).map(module => module.default)
             },
             {
-                date: "Jul 15, 2022",
+                date: "Jul 15, 2023",
+                note: "Decided to pack rope for the entire hike in 90+ degree weather. Forgot ATC anyways...",
                 images: Object.values(import.meta.glob('/src/data/adventure_images/smith_rock/07-15-23/*.jpg', { eager: true })).map(module => module.default)
             },
         ]
